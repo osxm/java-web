08075c127f8a9be37dd93042046dc5d73fadac1f
OSXM <oscar999@mail.ustc.edu.cn>
2023-08-11 23:05:53 +0800

    ok

---
+++ src/main/java/com/osxm/jw/servlet/EclipseFormatter.java
@@ -0,0 +1,35 @@
+/**  
+* @Title: EclipseFormatter.java
+* @Package com.osxm.jw.servlet
+* @Description: TODO
+* @author XM
+* @date 2023年8月5日 下午9:58:54
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.jw.servlet;
+
+/**
+ * @ClassName EclipseFormatter
+ * @Description TODO
+ * @author XM 
+ * @date 2023年8月5日
+ * 
+ */
+public class EclipseFormatter {
+
+	/**
+	 * 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+	 * 222222222222222222222222222
+	 * 333333333333
+	 * 
+	 * @param str
+	 */
+	public void formatTest(String str) {
+
+	}
+	
+	// @formatter:off
+	public void method2()   {  System.out.println("22");  }
+	// @formatter:on
+}
